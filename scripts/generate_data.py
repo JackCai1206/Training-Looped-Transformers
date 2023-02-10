419db0ee68a97ac566844ae97e54370125ab7709
jackcai1206 <zcai75@wisc.edu>
2023-02-10 13:14:25 -0500

    generate data script

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from os import path
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ))
 import argparse
@@ -6,23 +7,38 @@ import code
 from simulator import SubleqSim
 import torch
 from math import *
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser('Data generater for OISC input output pairs')
 parser.add_argument('-N', type=int, default=4, required=False, help='Number of bits for integers stored in memory column')
 parser.add_argument('-s', type=int, default=4, required=False, help='Number of scratch pad columns')
-parser.add_argument('-m', type=int, default=2, required=False, help='Number of memory locations')
-parser.add_argument('-n', type=int, default=8, required=False, help='Total number of columns')
-parser.add_argument('--num_train', type=int, default=100000, required=False, help='Number of training data points')
-parser.add_argument('--num_eval', type=int, default=5000, required=False, help='Number of evalutation data points')
-parser.add_argument('--output', type=str, default='', required=False, help='Output directory')
+parser.add_argument('-m', type=int, default=4, required=False, help='Number of memory locations')
+parser.add_argument('-n', type=int, default=16, required=False, help='Total number of columns')
+parser.add_argument('--num_train', type=int, default=10000, required=False, help='Number of training data points')
+parser.add_argument('--num_eval', type=int, default=500, required=False, help='Number of evalutation data points')
+parser.add_argument('--output', type=str, default='.', required=False, help='Output directory')
 
 args = parser.parse_args()
 
+if not path.exists(args.output):
+    os.makedirs(args.output)
 
-for i in range(args.num_train):
+train_data = []
+eval_data = []
+for i in tqdm(range(args.num_train)):
+    sim = SubleqSim(args.N, args.s, args.m, args.n)    
+    train_data.append(torch.stack([sim.X, sim.step()]))
+    sim.create_state()
+
+for i in tqdm(range(args.num_eval)):
     sim = SubleqSim(args.N, args.s, args.m, args.n)
+    eval_data.append(torch.stack([sim.X, sim.step()]))
+    sim.create_state()
 
-    print(sim.X)
-    print(sim.step())
+train_data = torch.stack(train_data)
+eval_data = torch.stack(eval_data)
+torch.save(train_data, path.join(args.output, 'train.pt'))
+torch.save(eval_data, path.join(args.output, 'eval.pt'))
 
-    break
+print(torch.load(path.join(args.output, 'train.pt')).shape)
+print(torch.load(path.join(args.output, 'eval.pt')).shape)
